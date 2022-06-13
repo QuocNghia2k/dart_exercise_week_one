5b700114d7c3cce5ea6c0cf644c706081ea4cc99
HoQuocNghia <nghiabs1.6@gmial.com>
2022-06-13 16:13:46 +0700

    fix lession two

--- lib/OOP/lessionTwo/TestVecter.dart
+++ lib/OOP/lessionTwo/TestVecter.dart
@@ -3,7 +3,7 @@ import 'package:dart_exercise_week_one/OOP/lessionTwo/Vector3D.dart';
 import 'Vector3D.dart';
 void main(List<String> args) {
   Vector3D v1 = new Vector3D(2, 2, 2);
-  Vector3D v2 ;
-  v2.Same(2);
-  
+  Vector3D v2  = Vector3D.same(12);
+  ;
+
 }
--- lib/OOP/lessionTwo/Vector3D.dart
+++ lib/OOP/lessionTwo/Vector3D.dart
@@ -1,14 +1,10 @@
 class Vector3D {
   double _x, _y, _z;
   Vector3D(this._x, this._y, this._z);
-  // ignore: non_constant_identifier_names
-  Vector3D(double a){
-    this._x=this._y=this._z =a;
-  }
-  Same(double a){
-    _x=_y=_z=a;
-  }
-  
+
+  Vector3D.same(double a):
+    _x=a, _y=a,_z=a;
+ 
   bool coincide(Vector3D vecter){
     if (this._x==vecter._x&&this._y==vecter._y&&this._z==vecter._z) {
       return true;
